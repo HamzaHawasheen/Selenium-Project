b032402017442ef64ac9384f1eb7ad5399152815
HamzaHawasheen <139491046+HamzaHawasheen@users.noreply.github.com>
2024-01-17 21:22:28 +0200

    Add files via upload

---
+++ TestCase5.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com/")
+time.sleep(3)
+driver.maximize_window()
+search_field = driver.find_element(By.ID, 'APjFqb')
+search_field.send_keys('AAUP')
+search_field.submit()
+time.sleep(3)
+search_field = driver.find_element(By.PARTIAL_LINK_TEXT, 'جامعة')
+search_field.click()
+time.sleep(3)
+search_field = driver.find_element(By.PARTIAL_LINK_TEXT, 'عرض')
+driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });", search_field)
+time.sleep(3)
+search_field.click()
+time.sleep(3)
+search_field = driver.find_element(By.PARTIAL_LINK_TEXT, '3')
+driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });", search_field)
+time.sleep(3)
+search_field.click()
+time.sleep(3)
+search_field = driver.find_element(By.PARTIAL_LINK_TEXT, 'فادي')
+driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });", search_field)
+time.sleep(3)
+search_field.click()
+search_field = driver.find_element(By.XPATH, '//*[@id="node-2074"]/div/div[2]/div/div[2]/div/div/p[5]')
+driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });", search_field)
+time.sleep(6)
+driver.close()
